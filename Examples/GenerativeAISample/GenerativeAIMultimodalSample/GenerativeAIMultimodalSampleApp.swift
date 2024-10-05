995eee3c5147dec712224b4fcebf47452f9c7ee8
wow800915 <wow800915@gmail.com>
2024-10-05 15:33:03 +0800

    Revert "新增唱頌缽的選項跟畫面"
    This reverts commit ae32bf9dd2735062f0bbab2d7ec8a3f8a125f97a.

--- Examples/GenerativeAISample/GenerativeAIMultimodalSample/GenerativeAIMultimodalSampleApp.swift
+++ Examples/GenerativeAISample/GenerativeAIMultimodalSample/GenerativeAIMultimodalSampleApp.swift
@@ -18,43 +18,9 @@ import SwiftUI
 struct GenerativeAIMultimodalSampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()  // 使用 TabView 作為主要介面
-        }
-    }
-}
-
-struct ContentView: View {
-    var body: some View {
-        TabView {
             NavigationStack {
                 PhotoReasoningScreen()
             }
-            .tabItem {
-                Label("AI算命", systemImage: "photo")
-            }
-            
-            NavigationStack {
-                SingingBowlScreen()  // 新增的畫面
-            }
-            .tabItem {
-                Label("唱頌缽", systemImage: "music.note")
-            }
         }
     }
-}
-
-struct SingingBowlScreen: View {
-    var body: some View {
-        VStack {
-            Text("這是唱頌缽的畫面")
-                .font(.title)
-                .padding()
-            Text("在這裡你可以體驗聲音療癒。")
-        }
-        .navigationTitle("唱頌缽")
-    }
-}
-
-#Preview {
-    ContentView()
 }
