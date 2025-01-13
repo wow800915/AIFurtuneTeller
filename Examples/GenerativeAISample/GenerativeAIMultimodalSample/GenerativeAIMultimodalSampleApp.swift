c20695e7e51e06e78bcf56af809f399da82cb4a3
wow800915 <wow800915@gmail.com>
2025-01-14 00:53:09 +0800

    修改app名稱

--- Examples/GenerativeAISample/GenerativeAIMultimodalSample/GenerativeAIMultimodalSampleApp.swift
+++ Examples/GenerativeAISample/GenerativeAIMultimodalSample/GenerativeAIMultimodalSampleApp.swift
@@ -24,7 +24,7 @@ struct GenerativeAIMultimodalSampleApp: App {
             NavigationStack {
                 VStack {
                     HStack {
-                        Text("陳老師 AI 人生鏡")
+                        Text("陳老師AI人生鏡")
                             .font(.title) // 字體縮小到 "title"
                             .padding(.leading)
                         
